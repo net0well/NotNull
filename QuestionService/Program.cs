d889461c98ca19162d0f97042e975e2b005fb853
Wellington Neto <wellingtonalneto@gmail.com>
2025-12-16 22:27:10 -0300

    adding migration

--- QuestionService/Program.cs
+++ QuestionService/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using QuestionService.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -14,6 +17,8 @@ builder.Services.AddAuthentication()
         options.Audience = "notnull";
     });
 
+builder.AddNpgsqlDbContext<QuestionDbContext>("questionDb");
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -25,5 +30,20 @@ if (app.Environment.IsDevelopment())
 
 app.MapControllers();
 app.MapDefaultEndpoints();
+
+
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+
+try
+{
+    var context =  services.GetRequiredService<QuestionDbContext>();
+    await context.Database.MigrateAsync();
+}
+catch (Exception e)
+{
+    var logger = services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(e, "An error occurred seeding the DB.");
+}
 
 app.Run();
