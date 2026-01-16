7b9c319608068d73129e48c096ee1c51afa46d42
Wellington Neto <wellingtonalneto@gmail.com>
2026-01-15 22:52:07 -0300

    making sure aspire still works for  continuous development

--- NotNull.AppHost/AppHost.cs
+++ NotNull.AppHost/AppHost.cs
@@ -11,7 +11,6 @@ var keycloak = builder.AddKeycloak("keycloak", 6001)
     .WithRealmImport("../infra/realms")
     .WithEnvironment("KC_HTTP_ENABLED", "true")
     .WithEnvironment("KC_HOSTNAME_STRICT", "false")
-    .WithEndpoint(6001, 8080, "keycloack", isExternal: true)
     .WithEnvironment("VIRTUAL_HOST", "id.notnull.local")
     .WithEnvironment("VIRTUAL_PORT", "8080");
 
