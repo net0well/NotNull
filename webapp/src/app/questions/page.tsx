a8699aa06f2d5e59bf09711f41a5b5e45f252f32
Wellington Neto <wellingtonalneto@gmail.com>
2026-01-19 20:47:53 -0300

    adding question header

--- webapp/src/app/questions/page.tsx
+++ webapp/src/app/questions/page.tsx
@@ -1,12 +1,15 @@
 ﻿import {getQuestions} from "@/lib/actions/question-actions";
 import QuestionCard from "@/app/questions/QuestionCard";
+import QuestionsHeader from "@/app/questions/QuestionsHeader";
+import {SearchParams} from "next/dist/server/request/search-params";
 
-export default async function QuestionsPage() {
-    const questions = await getQuestions();
-    
+export default async function QuestionsPage({searchParams}: {searchParams?: Promise<{tag?:string}>}) {
+    const params =  await searchParams;
+    const questions = await getQuestions(params?.tag);
     
     return (
         <>
+            <QuestionsHeader total={questions.length} tag={params?.tag}/>
                 {questions.map(question => (
                     <div key={question.id} className='py-4 not-last:border-b w-full flex'>
                         <QuestionCard key={question.id} question={question}/>
