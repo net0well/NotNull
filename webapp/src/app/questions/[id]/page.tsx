773d2315fc547860aabbc66dd0a92a7c8bfe99fa
Wellington Neto <wellingtonalneto@gmail.com>
2026-01-20 21:50:46 -0300

    adding question detailed page

---
+++ webapp/src/app/questions/[id]/page.tsx
@@ -0,0 +1,19 @@
+﻿import {getQuestionById} from "@/lib/actions/question-actions";
+import {notFound} from "next/navigation";
+import QuestionDetailedHeader from "@/app/questions/[id]/QuestionDetailedHeader";
+
+
+type Params = Promise<{ id: string }>
+
+export default async function QuestionDetailed({ params }: {params: Params}) {
+    const { id } = await params;
+    const question = await getQuestionById(id);
+
+    if (!question) return notFound();
+
+    return (
+        <div className='w-full'>
+            <QuestionDetailedHeader question={question} />
+        </div>
+    )
+}
