53d7653e52cc40b7e36982b78be1ab3fc08e3415
Wellington Neto <wellingtonalneto@gmail.com>
2026-01-25 17:57:38 -0300

    adding the answer content

---
+++ webapp/src/app/questions/[id]/QuestionContent.tsx
@@ -0,0 +1,21 @@
+﻿import { Question } from "@/lib/types";
+import {ArrowDownCircleIcon, ArrowUpCircleIcon } from "@heroicons/react/24/outline";
+import { Button } from "@heroui/button";
+import VotingButtons from "@/app/questions/[id]/VotingButtons";
+
+type Props = {
+    question: Question;
+}
+
+export default function QuestionContent({question} : Props) {
+    return (
+        <div className="border-b">
+            <div className='flex border-b border-gray-200 dark:border-gray-700 pb-3 px-6'>
+                <VotingButtons/>
+                <div className='flex-1 mt-4 ml-6 prose prose-gray dark:prose-invert max-w-none'
+                     dangerouslySetInnerHTML={{ __html: question.content }}
+                />
+            </div>
+        </div>
+    );
+}
---
+++ webapp/src/app/questions/[id]/VotingButtons.tsx
@@ -0,0 +1,38 @@
+﻿import {Button} from "@heroui/button";
+import {ArrowDownCircleIcon, ArrowUpCircleIcon, CheckIcon} from "@heroicons/react/24/outline";
+import { CheckBadgeIcon } from "@heroicons/react/24/solid";
+
+type Props = {
+    accepted?: boolean;
+}
+
+export default function VotingButtons({accepted}: Props){
+    return (
+        <div className='flex-shrink-0 flex flex-col gap-3 items-center justify-start mt-4'>
+            <Button
+                isIconOnly
+                variant='light'
+                className='text-gray-600 dark:text-gray-400 hover:text-purple-600 dark:hover:text-purple-400 hover:bg-purple-50 dark:hover:bg-purple-900/20'
+            >
+                <ArrowUpCircleIcon className='w-12' />
+            </Button>
+            <span className='text-xl font-semibold text-gray-800 dark:text-gray-200'>0</span>
+            <Button
+                isIconOnly
+                variant='light'
+                className='text-gray-600 dark:text-gray-400 hover:text-purple-600 dark:hover:text-purple-400 hover:bg-purple-50 dark:hover:bg-purple-900/20'
+            >
+                <ArrowDownCircleIcon className='w-12' />
+            </Button>
+            {accepted && (
+                <Button
+                isIconOnly
+                variant='light'
+                >
+                    <CheckBadgeIcon className='size-12 text-sucess' strokeWidth={4}/>
+                </Button>
+            )}
+        </div>
+    );
+}
+
