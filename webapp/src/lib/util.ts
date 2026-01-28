1b523001dc35aee2849cb94e3793d96f0009744d
Wellington Neto <wellingtonalneto@gmail.com>
2026-01-27 21:02:56 -0300

    testing client side error handling, added triggerError functionality for improve productions errors

---
+++ webapp/src/lib/util.ts
@@ -0,0 +1,43 @@
+﻿import {addToast} from "@heroui/toast";
+import {
+    differenceInCalendarDays,
+    differenceInCalendarMonths,
+    differenceInCalendarWeeks, formatDistanceToNow,
+    isToday,
+    isYesterday
+} from "date-fns";
+
+export function errorToast(error: {message: string, status?: number}) {
+    return addToast({
+        title: error.status || 'Error!',
+        description: error.message || 'Something went wrong!',
+        color: 'danger'
+    })
+}
+
+export function handleError(error: {message: string, status: number}) {
+    if (error.status === 500) {
+        throw error
+    } else {
+        return errorToast(error)
+    }
+}
+
+export function fuzzyTimeAgo(date: Date | string): string {
+    const now = new Date();
+    if (isToday(date)) return 'Today';
+    if (isYesterday(date)) return 'Yesterday';
+
+    const days = differenceInCalendarDays(now, date);
+    if (days < 7) return `${days} day${days > 1 ? 's' : ''} ago`;
+
+    const weeks = differenceInCalendarWeeks(now, date);
+    if (weeks < 4) return `${weeks} week${weeks > 1 ? 's' : ''} ago`;
+
+    const months = differenceInCalendarMonths(now, date);
+    return `${months} month${months > 1 ? 's' : ''} ago`;
+}
+
+export function timeAgo(date: Date | string): string {
+    return formatDistanceToNow(date, { addSuffix: true });
+}
