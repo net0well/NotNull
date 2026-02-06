b98e74049b0a85fc11b69c58b79561dc60b9b4c1
Wellington Neto <wellingtonalneto@gmail.com>
2026-02-05 21:53:16 -0300

    cache nextjs

--- webapp/src/lib/actions/tag-actions.ts
+++ webapp/src/lib/actions/tag-actions.ts
@@ -5,6 +5,6 @@ import {fetchClient} from "@/lib/fetchClient";
 import {Tag} from "@/lib/types";
 
 export async function getTags() {
-    return fetchClient<Tag[]>('/tags', 'GET');
+    return fetchClient<Tag[]>('/tags', 'GET', {cache: 'force-cache', next: {revalidate: 3}});
 }
 
